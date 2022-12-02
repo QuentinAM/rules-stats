cb6e523fc0b8dcf56c0625421b6ca33601050428
FoxT3c <quentin.abel-marceau@epita.fr>
2022-12-02 14:02:42 +0100

    feat(build)

--- build/server/nodes/1.js
+++ build/server/nodes/1.js
@@ -2,6 +2,6 @@ import * as module from '../entries/pages/__error.svelte.js';
 
 export { module };
 export const index = 1;
-export const file = '_app/immutable/pages/__error.svelte-4ff32af5.js';
-export const imports = ["_app/immutable/pages/__error.svelte-4ff32af5.js","_app/immutable/chunks/index-bed743de.js","_app/immutable/chunks/Translation-0f26202d.js","_app/immutable/chunks/singletons-eca981c1.js","_app/immutable/chunks/utils-a18431ba.js"];
+export const file = '_app/immutable/pages/__error.svelte-d2614d24.js';
+export const imports = ["_app/immutable/pages/__error.svelte-d2614d24.js","_app/immutable/chunks/index-bed743de.js","_app/immutable/chunks/Translation-77cc0d23.js","_app/immutable/chunks/singletons-eca981c1.js","_app/immutable/chunks/utils-a18431ba.js"];
 export const stylesheets = [];
